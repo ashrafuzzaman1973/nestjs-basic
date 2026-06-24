b545980a160655db227e601ab624e3b3371ca248
md.ashrafuzzaman <md.ashrafuzzaman@amiprobashi.com>
2026-06-24 14:11:37 +0600

    Partial Registration | Environment Variables

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,21 @@
 import { Injectable,Inject,forwardRef } from '@nestjs/common';
 import {UsersService} from "../user/users.service";
+import type { ConfigType } from '@nestjs/config';
+import authConfig from "./config/auth.config";
 
 @Injectable()
 export class AuthService {
-    constructor(@Inject(forwardRef(()=> UsersService)) private readonly userService : UsersService) {
+    constructor(
+        @Inject(forwardRef(()=> UsersService))
+        private readonly userService : UsersService,
+        @Inject(authConfig.KEY)
+        private readonly authConfiguration: ConfigType<typeof authConfig>,
+    ) {
     }
 
     isAuthenticated : Boolean = false ;
     login(email: string, password: string){
 
+        console.log(this.authConfiguration)
     }
 }
