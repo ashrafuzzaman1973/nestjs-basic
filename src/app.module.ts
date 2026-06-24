c3f96e8b7fa877f92af741707c8da9ac8b61daca
md.ashrafuzzaman <md.ashrafuzzaman@amiprobashi.com>
2026-06-24 13:30:54 +0600

    Custom Configuration File

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ProfileModule } from './profile/profile.module';
 import { HashtagModule } from './hashtag/hashtag.module';
 import {ConfigModule, ConfigService} from '@nestjs/config';
 import * as process from "node:process";
+import {appConfig} from "./config/app.config";
 
 const ENV = process.env.NODE_ENV;
 @Module({
@@ -18,20 +19,21 @@ const ENV = process.env.NODE_ENV;
       AuthModule,
       ConfigModule.forRoot({
         isGlobal: true,
-        envFilePath : !ENV ? '.env' : `.env.${ENV.trim()}`
+        envFilePath : !ENV ? '.env' : `.env.${ENV.trim()}`,
+        load : [appConfig]
       }),
     TypeOrmModule.forRootAsync({
     imports : [ConfigModule],
     inject : [ConfigService],
    useFactory : (configService:ConfigService) =>({
      type : "postgres",
-     autoLoadEntities : true,
-     synchronize : true,
-     host: configService.get('DB_HOST'),
-     port: +configService.get('DB_PORT'),
-     username: configService.get('DB_USERNAME'),
-     password: configService.get('DB_PASSWORD'),
-     database: configService.get('DB_NAME')
+     autoLoadEntities : configService.get('database.autoLoadEntities'),
+     synchronize : configService.get('database.synchronize'),
+     host: configService.get('database.host'),
+     port: configService.get('database.port'),
+     username: configService.get('database.username'),
+     password: configService.get('database.password'),
+     database: configService.get('database.name')
    })
   }), ProfileModule, HashtagModule],
   controllers: [AppController],
---
+++ src/config/app.config.ts
@@ -0,0 +1,16 @@
+import * as process from "node:process";
+
+export const appConfig = ()=>{
+    return {
+        environment : process.env.NODE_ENV || 'production',
+        database : {
+            host : process.env.DB_HOST || 'localhost',
+            port: Number(process.env.DB_PORT ?? 5432),
+            name : process.env.DB_NAME,
+            username : process.env.DB_USERNAME,
+            password : process.env.DB_PASSWORD,
+            synchronize: process.env.DB_SYNC === 'true' ? true : false,
+            autoLoadEntities : process.env.AUTO_LOAD === 'true' ? true : false,
+        }
+    }
+}
