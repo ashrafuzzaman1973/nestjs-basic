8ace22dc0a343cefe4dee88ef934ac3cf30bf1c4
md.ashrafuzzaman <md.ashrafuzzaman@amiprobashi.com>
2026-06-30 16:52:23 +0600

    custom guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import {Body, Controller, HttpCode, HttpStatus, Post} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {CreateUserDto} from "../user/dtos/create-user.dto";
 import {LoginDto} from "./dto/login.dto";
+import {AllowAnonymous} from "./decorators/allow-anonymous.decorator";
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @AllowAnonymous()
     @Post('login')
     @HttpCode(HttpStatus.OK)
     async login(@Body() loginDto: LoginDto) {
@@ -14,6 +16,7 @@ export class AuthController {
     }
 
 
+    @AllowAnonymous()
     @Post('signup')
     async signup(@Body() createUserDto: CreateUserDto) {
         return await this.authService.signup(createUserDto);
--- src/auth/guards/authorize.guard.ts
+++ src/auth/guards/authorize.guard.ts
@@ -2,15 +2,26 @@ import {CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException
 import {JwtService} from "@nestjs/jwt";
 import * as config from "@nestjs/config";
 import authConfig from "../config/auth.config";
+import {Reflector} from "@nestjs/core";
 
 @Injectable()
 export class AuthorizeGuard implements CanActivate {
     constructor(
         private readonly jwtService: JwtService,
         @Inject(authConfig.KEY)
-        private readonly authConfiguration: config.ConfigType<typeof authConfig>
+        private readonly authConfiguration: config.ConfigType<typeof authConfig>,
+        private readonly reflector: Reflector
     ) {}
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublic = this.reflector.getAllAndOverride('isPublic',[
+            context.getHandler(),
+            context.getClass()
+        ]);
+
+        if(isPublic){
+            return true;
+        }
+
         const request = context.switchToHttp().getRequest();
         const token = request.headers.authorization?.split(' ')[1];
         if(!token){
